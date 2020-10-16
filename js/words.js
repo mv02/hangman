1978deca93d850eea3ceac4b893a24fe4e8dad67
Milan <milanvodak02@gmail.com>
2020-10-16 11:37:51 +0200

    Add functions to pull and format words
    List of English words:
    https://github.com/bevacqua/correcthorse/blob/master/wordlist.json

---
+++ js/words.js
@@ -0,0 +1,24 @@
+let words;
+
+function getWords() {
+    return $.getJSON('https://raw.githubusercontent.com/bevacqua/correcthorse/master/wordlist.json', data => {
+        console.log(data);
+        words = data;
+    });
+}
+
+function newWord() {
+    let word;
+    do {
+        word = words[Math.floor(Math.random() * words.length)];
+    } while (word.length < 4)
+    return word;
+}
+
+function formatWord(str) {
+    let formatted = '';
+    for (let i = 0; i < str.length; i++) {
+        formatted += str[i] + ' ';
+    }
+    return formatted.trim();
+}
